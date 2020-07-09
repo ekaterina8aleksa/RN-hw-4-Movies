dfa01a6e15764644caaad3bd1c4913353b4f00e0
ekaterina8aleksa <ekaterina.palad87@gmail.com>
2020-07-09 04:10:41 +0300

    fetch fix

--- movies/src/services/fetchMovies.js
+++ movies/src/services/fetchMovies.js
@@ -5,21 +5,28 @@ const key = "d1c54d9ef388629bdcc2a11b2c88483f";
 
 export const fetchTrendsMovies = () => {
   return axios
-    .get(`${root}/trending/movie/day&api_key=${key}`)
+    .get(`${root}trending/movie/day?api_key=${key}`)
     .then((response) => response.data.results);
 };
 export const fetchSearch = (query) => {
   return axios
-    .get(`${root}/movie/&query=${query}&api_key=${key}&page=1`)
+    .get(
+      `${root}movie/search?api_key=${key}&language=en-US&query=${query}&page=1&include_adult=false`
+    )
     .then((response) => response.data.results);
 };
 export const fetchMovies = (movieId) => {
   return axios
-    .get(`${root}/movie/${movieId}&api_key=${key}`)
+    .get(`${root}movie/${movieId}?api_key=${key}&language=en-US`)
     .then((response) => response.data);
 };
-export const fetchCredits = (movieId) => {
+export const fetchCast = (movieId) => {
   return axios
-    .get(`${root}/movie/${movieId}/credits&api_key=${key}`)
+    .get(`${root}movie/${movieId}/credits?api_key=${key}`)
+    .then((response) => response.data.results);
+};
+export const fetchReviews = (movieId) => {
+  return axios
+    .get(`${root}movie/${movieId}/reviews?api_key=${key}&language=en-US&page=1`)
     .then((response) => response.data.results);
 };
